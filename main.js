e3290f194a7c8668e0c32997c41c5dfb66912a20
staceymw <116839832+staceymw@users.noreply.github.com>
2023-01-13 11:34:01 -0500

    Write reset function and add event listener in html file

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 let color = "black";
-let click = "true"
+let click = "true";
+
 
 function compileBoard(size) {
-    const board = document.querySelector(".board");
-    const squares = board.querySelectorAll("div");
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    const amount = size * size;
-    for (i=0; i < amount; i++) {
+    let amount = size * size;
+    for (i = 0; i < amount; i++) {
         const square = document.createElement("div");
-        square.style.backgroundColor = "blue";
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
         square.addEventListener("mouseover", (colorSquare));
     }
 }
 
- compileBoard(16);
+compileBoard(16);
 
 function changeSize(input) {
     if (input >= 2 && input <= 100) {
-    compileBoard(input);
+        compileBoard(input);
     } else {
         console.log("Number too high or low");
     }
@@ -37,6 +38,24 @@ function colorSquare() {
     }
 }
 
+
 function changeColor(choice) {
     color = choice;
 }
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
+        click = !click;
+        if (click) {
+            document.querySelector(".mode").textContent = "Mode: Coloring";
+        } else {
+            document.querySelector(".mode").textContent = "Mode: Not Coloring";
+        }
+    }
+});
